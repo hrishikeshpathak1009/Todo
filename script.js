b6f29f49dadd3f469c2c31dacb2938149337dd57
Hrishikesh Pathak <hrishikeshpathak1009@gmail.com>
2026-05-24 13:38:10 +0530

    basic graph added + UI fix

--- script.js
+++ script.js
@@ -18,8 +18,12 @@ const savedTasks = localStorage.getItem("tasks");
 
 const tasks = savedTasks ? JSON.parse(savedTasks) : [];
 
-let currentFilter = "all";
+const intervalInput =document.querySelector("#intervalInput");
 
+const chartCanvas =document.querySelector("#productivityChart");
+
+let currentFilter = "all";
+let productivityChart = null;
 renderTasks();
 
 //BUTTONS EVENT TRIGGER
@@ -37,7 +41,8 @@ button.addEventListener("click", function () {
         id: Date.now(),
         text: taskText,
         completed: false,
-        priority: priority
+        priority: priority,
+        completedAt: null
     });
     saveTasks();
 
@@ -73,6 +78,14 @@ pendingBtn.addEventListener("click", function () {
 
 });
 
+intervalInput.addEventListener(
+    "input",
+    function () {
+
+        renderChart();
+
+    }
+);
 
 //FUNCTIONS BELOW
 
@@ -84,8 +97,13 @@ function saveTasks() {
 
 
 function renderTasks() {
-    console.log(tasks);
+    const interval =
+    Number(intervalInput.value);
 
+console.log(
+    getProductivityData(interval)
+);
+    
     taskList.innerHTML = "";
 
     const completedTasks = tasks.filter(function (task) {
@@ -146,7 +164,19 @@ function renderTasks() {
 
         const li = document.createElement("li");
 
-        li.textContent =`${task.text} (Priority: ${task.priority})`;;
+        if (task.priority === 3) {
+    li.style.borderLeft = "5px solid red";
+}
+
+if (task.priority === 2) {
+    li.style.borderLeft = "5px solid orange";
+}
+
+if (task.priority === 1) {
+    li.style.borderLeft = "5px solid green";
+}
+
+        li.textContent =`${task.text} (Priority: ${getPriorityLabel(task.priority)})`;;
 
         const deleteBtn = document.createElement("button");
 
@@ -173,8 +203,9 @@ function renderTasks() {
         });
 
         if (task.completed) {
-            li.style.color="green";
-            li.style.textDecoration = "line-through";
+           
+            li.classList.add("completed-task");
+            
         }
 
 
@@ -184,11 +215,129 @@ function renderTasks() {
         li.addEventListener("click", function () {
 
             task.completed = !task.completed;
+
+            if(task.completed){
+                task.completedAt= new Date().toISOString();
+            }
+            else {
+                task.completedAt=null;
+            }
             saveTasks();
             renderTasks();
             
         });
 
     }
+    renderChart();
+
+}
+
+function getPriorityLabel(priority) {
+
+    if (priority === 1) {
+        return "Low";
+    }
+
+    if (priority === 2) {
+        return "Medium";
+    }
+
+    return "High";
+
+}
+
+function getProductivityData(intervalMinutes) {
+
+    const totalBuckets = Math.ceil((24 * 60) / intervalMinutes);
+
+    const productivityData =new Array(totalBuckets).fill(0);
+
+    for (let task of tasks) {
+        if (task.completed && task.completedAt) {
+            const date = new Date(task.completedAt);
+
+const totalMinutes =
+    date.getHours() * 60 +
+    date.getMinutes();
+
+const bucketIndex =
+    Math.floor(totalMinutes / intervalMinutes);
+
+productivityData[bucketIndex] += task.priority;
+        }
+
+    }
+    return productivityData;
+
+}
+
+
+function renderChart() {
+    const interval = Number(intervalInput.value);
+const labels=[];
+    const data = getProductivityData(interval);
+    for (let i = 0; i < data.length; i++) {
+
+    const totalMinutes = i * interval;
+
+    const hour =
+        Math.floor(totalMinutes / 60);
+
+    const minutes =
+        totalMinutes % 60;
+
+        
+    labels.push(
+        `${hour}:${minutes
+            .toString()
+            .padStart(2, "0")}`
+    );
+
+}
+    if (productivityChart) {
+
+    productivityChart.destroy();
+
+}
+
+productivityChart = new Chart(chartCanvas, {type: "line",data: {
+
+    labels: labels,
+
+    datasets: [
+
+        {
+            label: "Productivity",
+
+            data: data,
+
+            borderColor: "#00ffae",
+
+            backgroundColor:
+                "rgba(0,255,174,0.2)",
+
+            tension: 0.3
+        }
+
+    ]
+
+},options: {
+
+    responsive: true,
+
+    scales: {
+
+        y: {
+
+            beginAtZero: true
+
+        }
+
+    }
+
+}
+});
+
+
 
 }
